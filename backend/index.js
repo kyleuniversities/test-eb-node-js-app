c67f36abfaea9210206b244f6e7669010e3a4325
kyleuniversities <kyleuniversities@gmail.com>
2024-01-30 19:16:57 -0800

    used fullstack user app

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,15 @@ const cors = require('cors');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
+const knexfile = require('./knexfile');
+const { Model } = require('objection');
 
 // Set up express
 const app = express();
+
+// Set up knex
+const knex = require('knex')(knexfile.development);
+Model.knex(knex);
 
 // Set up express middleware
 app.use(jsonParser);
@@ -17,22 +23,18 @@ app.use('/', router);
 port = 8080;
 
 // Set up test GET endpoint
-router.get('/api', function (req, res) {
-  return res.send(`Get Hello World!`);
-});
-
-// Set up test GET endpoint
 router.get('/api/test', function (req, res) {
-  return res.send(`Get Hello World Test!`);
+  return res.send(`Get Hello World!`);
 });
 
 // Set up test POST endpoint
 router.post('/api/test', function (req, res) {
-  return res.send(`Post Hello World Test!`);
+  return res.send(`Post Hello World!`);
 });
 
-// Use web router
-router.use('/', require('./web-router'));
+// Mount routes
+app.use('/api/users/', require('./router/user-router'));
+app.use('/', require('./web-router'));
 
 // Launch app
 app.listen(port, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ViewUsersPage } from './app/component/user/ViewUsersPage';
+import { RegisterUserPage } from './app/component/user/RegisterUserPage';
+import { EditUserPage } from './app/component/user/EditUserPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<ViewUsersPage />} />
+        <Route path="/registration" element={<RegisterUserPage />} />
+        <Route path="/users/edit/:id" element={<EditUserPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
